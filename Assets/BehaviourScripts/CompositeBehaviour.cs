beb5deefa6fc328b4caccfc178be64b28c707e3c
Keyrune <spyro-99@mail.ru>
2021-03-23 17:44:32 +0300

    add default behaviour

---
+++ Assets/BehaviourScripts/CompositeBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behaviour/Composite")]
+public class CompositeBehaviour : FlockBehaviour
+{
+    public FlockBehaviour[] behaviours;
+    public float[] weigths;
+
+    public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        if (weigths.Length != behaviours.Length)
+        {
+            Debug.LogError("Data mismatch in " + name, this);
+            return Vector2.zero;
+        }
+
+        //set up move
+        Vector2 move = Vector2.zero;
+
+        //iterate thru behaviours
+        for (int i = 0; i < behaviours.Length; i++)
+        {
+            Vector2 partialMove = behaviours[i].CalculateMove(agent, context, flock) * weigths[i];
+
+            if (partialMove != Vector2.zero)
+            {
+                if (partialMove.sqrMagnitude > weigths[i] * weigths[i])
+                {
+                    partialMove = partialMove.normalized * weigths[i];
+                }
+
+                move += partialMove;
+            }
+        }
+
+        return move;
+    }
+}
